d590eb7936fb3e6e38dcaafae7e808ee492d3eba
yayararakanayana-beep <yayararakanayana@gmail.com>
2026-07-11 10:59:46 +0900

    Export repaired Stage B/C interfaces

--- scripts/task3_1f_structure_extraction/__init__.py
+++ scripts/task3_1f_structure_extraction/__init__.py
@@ -2,6 +2,7 @@
 from .contract import DEFAULT_CONTRACT, OUTPUT_SUBDIR, load_contract
 from .input_freeze import freeze_input
 from .models import (
+    fit_weighted_frobenius_nmf,
     fit_weighted_kl_nmf,
     fit_weighted_pca,
     match_components,
@@ -10,7 +11,10 @@ from .models import (
     transform_fixed_kl_basis,
 )
 from .runner import run_smoke
-from .batch import formal_run_plan, grouped_subsets, representative_runs, run_plan, run_stage_bc_smoke, select_rank
+from .stage_bc_run import formal_run_plan, run_plan
+from .stage_bc_subsets import grouped_subsets
+from .stage_bc_selection_core import representative_runs, select_rank
+from .batch import run_stage_bc, run_stage_bc_formal, run_stage_bc_smoke
 from .selection_validator import validate_selection
 from .validator import validate_smoke
 
